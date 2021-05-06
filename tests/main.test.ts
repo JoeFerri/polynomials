7a71e0e1e800a1a238e3a5c6fd9de61b4dbab952
Giuseppe Ferri <46257651+JoeFerri@users.noreply.github.com>
2021-05-06 08:58:30 +0200

    Add files via upload

---
+++ tests/main.test.ts
@@ -0,0 +1,21 @@
+/**
+ * @author Giuseppe Ferri
+ */
+
+
+
+import * as $$ from "../lib/index";
+//  import * as fs from "fs";
+import { expect, should } from 'chai';
+should();
+ 
+ 
+ 
+describe(`...`, function() {
+
+  it(`...`, function() {
+    "test".should.to.be.equal("test");
+  });
+ 
+});
+ 
