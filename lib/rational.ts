d54e0cdcc728008b50572b741ef0a1470de7743d
Giuseppe Ferri <46257651+JoeFerri@users.noreply.github.com>
2021-05-08 09:15:48 +0200

    license, mail, copyright

--- lib/rational.ts
+++ lib/rational.ts
@@ -1,5 +1,8 @@
 /**
  * @author Giuseppe Ferri
+ * @license LGPL-3.0
+ *
+ * Copyright (c) 2021, Giuseppe Ferri (joeferri83prog@libero.it)
  */
 
 import { UndefinedError } from "./error";
---
+++ tests/complex.test.ts
@@ -0,0 +1,22 @@
+/**
+ * @author Giuseppe Ferri
+ * @license LGPL-3.0
+ *
+ * Copyright (c) 2021, Giuseppe Ferri (joeferri83prog@libero.it)
+ */
+
+
+
+import * as $$ from "../lib/index";
+import { expect, should } from 'chai';
+should();
+
+
+
+describe(`Complex`, function() {
+
+  it(`...`, function() {
+    "test".should.to.be.equal("test");
+  });
+
+});
