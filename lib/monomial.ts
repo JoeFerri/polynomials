04a4301c3cc8f6db70524ac8c9a13f56424c258f
Giuseppe Ferri <46257651+JoeFerri@users.noreply.github.com>
2021-05-11 16:15:04 +0200

    polynomials/issues/24#issue-880674556

--- lib/monomial.ts
+++ lib/monomial.ts
@@ -5,7 +5,7 @@
  * Copyright (c) 2021, Giuseppe Ferri (joeferri83prog@libero.it)
  */
 
-import { charindexnum } from "./char";
+import { charindexnum, charindexnumOpts, cinopt } from "./char";
 import { Complex } from "./complex";
 import { ExpLiteral } from "./expliteral";
 import { undnumber } from "./type";
@@ -24,25 +24,27 @@ export class Monomial {
   }
 
 
-  value(cns: charindexnum[] = []) : undnumber {
+  value(cns: charindexnum[]|cinopt[] = []) : undnumber {
+    cns = charindexnumOpts(cns);
     let
       vz = this.z.value(),
       vl = this.literals
         .map( l => l.value(cns) )
-        .reduce( (prev,curr) => prev != undefined && curr != undefined ? prev * curr : undefined);
+        .reduce( (prev,curr) => prev != undefined && curr != undefined ? prev * curr : undefined, 1);
     return vz != undefined && vl != undefined ? vz * vl : undefined;
   }
 
   
   private track: string|undefined = undefined;
 
+
   literalsTrack() : string {
     if (this.track == undefined)
       this.track =
         this.literals
+          .sort(literalsSort)
           .map( l => l.toString() )
-          .sort( (l1,l2) => l1.localeCompare(l2) )
-          .join('');
+          .join(Monomial.dotChar);
     return this.track;
   }
 
@@ -59,6 +61,76 @@ export class Monomial {
 
 
   toString() : string {
-    return this.z.toString() + this.literalsTrack();
+    let track = this.literalsTrack();
+    return this.z.toString() + (track != '' ? Monomial.dotChar + track : '');
   }
+
+
+  private static readonly dotList = [
+    '',
+    '⋅',     // DOT OPERATOR          (U+22C5)
+    '·',    // MIDDLE DOT            (U+00B7)
+    '·',   // GREEK ANO TELEIA      (U+0387)
+    '×',  // MULTIPLICATION SIGN   (U+00D7)
+    '*', // ASTERISK              (U+002A)
+    '﹡', // SMALL ASTERISK        (U+FE61)
+  ];
+  
+
+  static get dotChar() : string {
+    return Monomial.dotList[Monomial.dotCode];
+  }
+
+
+  private static _dotCode: 0|1|2|3|4|5|6 = 1;
+  
+
+  static get dotCode(): 0|1|2|3|4|5|6 {
+    return Monomial._dotCode;
+  }
+
+
+  static set dotCode(id: 0|1|2|3|4|5|6) {
+    Monomial._dotCode = id;
+  }
+
+
+  static readonly zero      = new Monomial(Complex.zero);
+  static readonly one       = new Monomial(Complex.one);
+  static readonly mone      = new Monomial(Complex.mone);
+  static readonly infinity  = new Monomial(Complex.infinity);
+  static readonly minfinity = new Monomial(Complex.minfinity);
+  
+  static readonly x = new Monomial(Complex.one, ExpLiteral.x);
+  static readonly y = new Monomial(Complex.one, ExpLiteral.y);
+  static readonly k = new Monomial(Complex.one, ExpLiteral.k);
+  static readonly z = new Monomial(Complex.one, ExpLiteral.z);
+  
+  static readonly x2 = new Monomial(Complex.one, new ExpLiteral('x',undefined,2));
+  static readonly y2 = new Monomial(Complex.one, new ExpLiteral('y',undefined,2));
+  static readonly z2 = new Monomial(Complex.one, new ExpLiteral('z',undefined,2));
+  
+  static readonly x3 = new Monomial(Complex.one, new ExpLiteral('x',undefined,3));
+  static readonly y3 = new Monomial(Complex.one, new ExpLiteral('y',undefined,3));
+  static readonly z3 = new Monomial(Complex.one, new ExpLiteral('z',undefined,3));
+}
+
+
+function literalsSort(l1: ExpLiteral, l2: ExpLiteral) : number { 
+  let locCompChar = l1.char.localeCompare(l2.char);
+  let locCompIndex = l1.index - l2.index;
+
+  // x != y
+  // alphabetical ordering
+  if (locCompChar != 0)
+    return locCompChar;
+
+  // x_1 != x_2
+  // lexicographic ordering
+  if (locCompIndex != 0)
+    return locCompIndex;
+  
+  // x_1^3 != x_1^2 != x_1
+  // major powers on the left
+  return l2.exp.value() - l1.exp.value();
 }
---
+++ tests/monomial.test.ts
@@ -0,0 +1,114 @@
+/**
+ * @author Giuseppe Ferri
+ * @license LGPL-3.0
+ *
+ * Copyright (c) 2021, Giuseppe Ferri (joeferri83prog@libero.it)
+ */
+
+
+
+import * as $$ from "../lib/index";
+import { expect, should } from 'chai';
+import { ExpRational } from "../lib/index";
+should();
+
+
+
+describe(`Monomial`, function() {
+
+  let
+    cns: $$.charindexnum[] = [],
+    cin: $$.cinopt[] = [],
+
+    x = $$.ExpLiteral.x,
+    x2 = new $$.ExpLiteral('x',undefined,2),
+    x3 = new $$.ExpLiteral('x',undefined,3),
+    y = $$.ExpLiteral.y,
+    x_1 = new $$.ExpLiteral('x',1),
+    x_2 = new $$.ExpLiteral('x',2),
+    x_3 = new $$.ExpLiteral('x',3,5),
+    
+    z2 = new $$.Complex(new ExpRational(2)),
+    zm3 = new $$.Complex(new ExpRational(-3)),
+    z23 = new $$.Complex(new ExpRational(2,3)),
+
+    mono1 = new $$.Monomial(z2),                //? 2
+    mono2 = new $$.Monomial(zm3),               //? -3
+    mono3 = new $$.Monomial(z23),               //? 2/3
+    mono4 = new $$.Monomial(z2,x,y),            //? 2⋅x⋅y
+    mono5 = new $$.Monomial(zm3,x,x3,x2),       //? -3⋅x^3⋅x^2⋅x
+    mono6 = new $$.Monomial(z23,x_1,x_2),       //? 2/3⋅x_1⋅x_2
+    mono7 = new $$.Monomial(z23,x_1,x_2,x_3),   //? 2/3⋅x_1⋅x_2⋅x_3^5
+    
+    zero = $$.Monomial.zero,
+    one = $$.Monomial.one,
+    mone = $$.Monomial.mone,
+    infinity = $$.Monomial.infinity,
+    minfinity = $$.Monomial.minfinity;
+
+  it(`#value()`, function() {
+    expect(mono1.value()).to.be.equal(2);
+    expect(mono2.value()).to.be.equal(-3);
+    expect(mono3.value()).to.be.equal(2/3);
+
+    expect(mono4.value()).to.be.undefined;
+    expect(mono5.value()).to.be.undefined;
+    expect(mono6.value()).to.be.undefined;
+
+    expect(zero.value()).to.be.equal(0);
+    expect(one.value()).to.be.equal(1);
+    expect(mone.value()).to.be.equal(-1);
+    expect(infinity.value()).to.be.equal(Infinity);
+    expect(minfinity.value()).to.be.equal(-Infinity);
+    
+    cns = [['x',0,1],['y',0,2],['x',1,3]];
+    expect(mono4.value(cns)).to.be.equal(4); //? 2⋅x⋅y → 2⋅1⋅2 = 4
+    cns = [['x',1,1],['x',2,2],['x',0,3]];   //? 2⋅x⋅y → 2⋅3⋅y = undefined
+    expect(mono4.value(cns)).to.be.undefined;
+    cns = [['x',0,2]];
+    expect(mono5.value(cns)).to.be.equal(-192); //? -3⋅x^3⋅x^2⋅x → -3⋅2^3⋅2^2⋅2 = -3⋅8⋅4⋅2 = -192
+    cns = [['x',1,1],['x',2,2],['x',0,2]];
+    expect(mono5.value(cns)).to.be.equal(-192); //? -3⋅x^3⋅x^2⋅x → -3⋅2^3⋅2^2⋅2 = -3⋅8⋅4⋅2 = -192
+    cns = [['x',1,1],['x',2,2],['x',0,3]];
+    expect(mono6.value(cns)).to.be.equal(4/3); //? 2/3⋅x_1⋅x_2 → 2/3⋅1⋅2 = 4/3
+
+    cin = [{c:'x',i:0,n:1},{c:'y',n:2},{c:'x',i:1,n:3}];
+    expect(mono4.value(cin)).to.be.equal(4); //? 2⋅x⋅y → 2⋅1⋅2 = 4
+  });
+
+  it(`#equals()`, function() {
+    zero.equals(zero).should.to.be.equal(true);
+    one.equals(one).should.to.be.equal(true);
+    one.equals(mone).should.to.be.equal(false);
+    mone.equals(mone).should.to.be.equal(true);
+    infinity.equals(infinity).should.to.be.equal(true);
+    minfinity.equals(minfinity).should.to.be.equal(true);
+
+    mono4.equals(mono4).should.to.be.equal(true);
+    mono4.equals(mono5).should.to.be.equal(false);
+  });
+
+  it(`#toString()`, function() {
+    zero.toString().should.to.be.equal("0");
+    one.toString().should.to.be.equal("1");
+    mone.toString().should.to.be.equal("-1");
+    infinity.toString().should.to.be.equal("infinity");
+    minfinity.toString().should.to.be.equal("-infinity");
+
+    mono1.toString().should.to.be.equal("2");
+    mono2.toString().should.to.be.equal("-3");
+    mono3.toString().should.to.be.equal("2/3");
+    mono4.toString().should.to.be.equal("2⋅x⋅y");
+    mono5.toString().should.to.be.equal("-3⋅x^3⋅x^2⋅x");
+    mono6.toString().should.to.be.equal("2/3⋅x_1⋅x_2");
+    mono7.toString().should.to.be.equal("2/3⋅x_1⋅x_2⋅x_3^5");
+  });
+
+});
+
+// mono1 = new $$.Monomial(z2),            //? 2
+// mono2 = new $$.Monomial(zm3),           //? -3
+// mono3 = new $$.Monomial(z23),           //? 2/3
+// mono4 = new $$.Monomial(z2,x,y),        //? 2⋅x⋅y
+// mono5 = new $$.Monomial(zm3,x,x3,x2),   //? -3⋅x^3⋅x^2⋅x
+// mono6 = new $$.Monomial(z23,x_1,x_2),   //? 2/3⋅x_1⋅x_2
