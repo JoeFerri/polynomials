c4d65fb7056c568e6e56307d40581bc487bcb9ef
Giuseppe Ferri <46257651+JoeFerri@users.noreply.github.com>
2023-12-23 17:47:26 +0100

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,7 +2,7 @@
  * @author Giuseppe Ferri
  * @license LGPL-3.0
  *
- * Copyright (c) 2021, Giuseppe Ferri (joeferri83prog@libero.it)
+ * Copyright (c) 2021, Giuseppe Ferri <jfinfoit@gmail.com>
  */
 
 
